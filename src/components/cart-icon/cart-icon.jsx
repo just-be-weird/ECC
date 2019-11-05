6c5505edd1bc4a342a44215447c0895bfc2e25c2
just-be-weird <abhi.chandresh@gmail.com>
2019-11-05 16:07:10 +0530

    Code for showing total count on cart icon

--- src/components/cart-icon/cart-icon.jsx
+++ src/components/cart-icon/cart-icon.jsx
@@ -5,17 +5,22 @@ import './cart-icon.styles.scss';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ itemCount, toggleCartHidden }) => (
 	<div className='cart-icon' onClick={toggleCartHidden}>
 		<ShoppingIcon className='shopping-icon' />
-		<span className='item-count'>0</span>
+		<span className='item-count'>{itemCount}</span>
 	</div>
 );
 
 CartIcon.propTypes = {
-	toggleCartHidden: PropTypes.func.isRequired
+	toggleCartHidden: PropTypes.func.isRequired,
+	itemCount: PropTypes.number.isRequired
 };
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+	//selector-as it pulls of small slice of state
+	itemCount: cartItems.reduce((accQty, cartItem) => accQty + cartItem.quantity, 0)
+});
 const mapDispatchToProps = (dispatch) => ({
 	toggleCartHidden: () => dispatch(toggleCartHidden())
 });
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
