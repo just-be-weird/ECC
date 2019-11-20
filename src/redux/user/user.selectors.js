f6329e413e9b813f2a2ac010160314e01ef3b9ed
just-be-weird <abhi.chandresh@gmail.com>
2019-11-20 18:21:10 +0530

    Added User Selector and uodated header comp

--- src/redux/user/user.selectors.js
+++ src/redux/user/user.selectors.js
@@ -1,5 +1,5 @@
 import { createSelector } from 'reselect';
 
 const selectUser = (state) => state.user;
-//typo in return will prevent rendring
+//typo in return will prevent rendring  || first argument can be , seperated array or simply , seperated values
 export const selectCurrentUser = createSelector([ selectUser ], (user) => user.currentUser);
