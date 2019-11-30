f31731b9ab6a6cc80c60d5e0422d2d6fadb4c3e5
just-be-weird <abhi.chandresh@gmail.com>
2019-11-30 21:37:54 +0530

    Updated the code with redux thunk and used in shop component to fetch async data and dispatch multiple actions
    Signed-off-by: just-be-weird <abhi.chandresh@gmail.com>

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,33 @@
-import {UPDATE_COLLECTIONS} from '../actionTypes';
+import {FETCH_COLLECTIONS_FAILURE, FETCH_COLLECTIONS_START, FETCH_COLLECTIONS_SUCCESS} from '../actionTypes';
+import {convertCollectionsSnapshotToMap, firestore} from '../../firebase/firebase.utils';
 
-export const updateCollections = (collectionsMap) => ({
-  type: UPDATE_COLLECTIONS,
-  payload: collectionsMap
+//All thunks action creator that returns a function that gets a dispatch  | action === JS object
+//step1 is create a actionGenerator function which will be returning one
+export const fetchCollectionsStart = () => ({
+  type: FETCH_COLLECTIONS_START,
 });
+
+export const fetchCollectionSuccess = collectionsMap => ({
+  type: FETCH_COLLECTIONS_SUCCESS,
+  payload: collectionsMap,
+});
+
+export const fetchCollectionsFailure = errorMessage => ({
+  type: FETCH_COLLECTIONS_FAILURE,
+  payload: errorMessage,
+});
+
+//#Concept: Actual function that will be passed to components that will begin the process call it as ActionGenerator Fn
+export const fetchCollectionsStartAsync = () => {
+  // returns a function, this function gets a dispatch as a first argument
+  //In this function we can use dispatch to fire multiples action firing functions
+  return dispatch => {
+    const collectionRef = firestore.collection('collections');
+    dispatch(fetchCollectionsStart());
+
+    collectionRef.get().then(snapshot => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      dispatch(fetchCollectionSuccess(collectionsMap));
+    }).catch((error) => dispatch(fetchCollectionsFailure(error.message)));
+  }
+};
